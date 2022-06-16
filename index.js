46bfb521b91b27a0d3b6546219e9730aa3b934de
Alex Aguirre <teamaguirre@gmail.com>
2022-06-16 09:25:24 -0700

    Refatored the JS Code for readability

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-function newImage(url, left, bottom){
+function newImage(url){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
-    document.body.append(image)
+        document.body.append(image)
     return image
 }
 
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
+// Create a Move Function
+function move(image, left, bottom){
+    image.style.position = 'fixed'
+    
+
+    function moveToCoordinates(left, bottom){
+        image.style.left = left + 'px'
+        image.style.bottom = bottom + 'px'
+    }
+    return {
+        to:moveToCoordinates
+    }
+}
+
+
+let greenCharacter=newImage('assets/green-character.gif')
+move(greenCharacter).to(100,200)
+
+let tree=newImage('assets/tree.png')
+move(tree).to(200, 450)
+let pillar =newImage('assets/pillar.png')
+move (pillar).to(350, 250)
+
+let pinetree = newImage('assets/pine-tree.png')
+move(pinetree).to(450, 350)
+
+let crate = newImage('assets/crate.png')
+move(crate).to(150, 350)
+
+let well = newImage('assets/well.png')
+move(well).to(500, 575)
 
 
 function newItem(url, left, bottom){
